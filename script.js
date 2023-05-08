7bf44f463f1786e7a9993f164bbc44c75b5d4383
irfanyahyaa <irfanyahyaa@gmail.com>
2023-05-08 12:17:52 +0700

    Finishing operator at js

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
-
 function plus() {
     let x = document.getElementById("numb1").value;
     let y = document.getElementById("numb2").value;
 
     let plus = parseInt(x) + parseInt(y);
-    plus.onclick = result;
+    // alert(plus);
+    document.getElementById("result").value = plus
 }
 
-function result () {
-    document.getElementById("result").innerHTML = 
+function minus() {
+    let x = document.getElementById("numb1").value;
+    let y = document.getElementById("numb2").value;
+
+    let minus = parseInt(x) - parseInt(y);
+    // alert(minus);
+    document.getElementById("result").value = minus
 }
 
-// function plus() {
-//     let plus = document.getElementById("numb1").value;
-//     alert(plus);
-// }
+function times() {
+    let x = document.getElementById("numb1").value;
+    let y = document.getElementById("numb2").value;
+
+    let times = parseInt(x) * parseInt(y);
+    // alert(times);
+    document.getElementById("result").value = times
+}
+
+function devide() {
+    let x = document.getElementById("numb1").value;
+    let y = document.getElementById("numb2").value;
+
+    let devide = parseInt(x) / parseInt(y);
+    // alert(devide);
+    document.getElementById("result").value = devide
+}
